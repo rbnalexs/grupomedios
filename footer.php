d7dd83de5a62b9dc86c40e66206fcc99f50455ee
Ruben Sanchez <rubensanchezcolin@gmail.com>
2025-06-27 12:09:26 -0600

    Opmizacion CSS

--- footer.php
+++ footer.php
@@ -32,7 +32,7 @@
 
 
     <div class="col-md-6 foot derecha">
-      <span class="edGratis"><a href="https://ginmedia.com.mx/descarga-revistas-gratis">Edicion digital GRATIS</a></span><br>
+      <span class="edGratis"><a href="https://ginmedia.com.mx/descarga-revistas-gratis" target="_blank">Edicion digital GRATIS</a></span><br>
       <span class="titFooter">Suscripciones</span><br>
       <span>
         suscripciones@mundoejecutivo.com.mx<br>
@@ -45,7 +45,7 @@
 
 <div class="stickyBanner">
   <div class="closeSticky"><i class="fas fa-times"></i></div>
-  <img src="images/sticky.png">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/sticky.png">
 </div>
 
 
@@ -54,7 +54,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
 
 </html>
--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600&family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.min.css">
   <?php wp_head(); ?>
 </head>
 
